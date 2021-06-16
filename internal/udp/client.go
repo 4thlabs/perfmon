aa4417c41678b744656a9dd00b4393d9b93579a1
Thomas Gourgues <thomas.gourgues@gmail.com>
2021-06-16 12:12:26 +0200

    Added encryption

--- internal/udp/client.go
+++ internal/udp/client.go
@@ -1,9 +1,13 @@
 package udp
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/hex"
+	"io"
 	"log"
 	"net"
-	"runtime"
 
 	"gitlab.com/4thlabs/perfmon/internal/recording"
 )
@@ -31,9 +35,35 @@ func (client *Client) Connect(address string, port int) error {
 	return nil
 }
 
-func (client *Client) Start(recording *recording.Recording) {
+func encryt(key []byte, data []byte) []byte {
+	// Load your secret key from a safe place and reuse it across multiple
+	// Seal/Open calls. (Obviously don't use this example key for anything
+	// real.) If you want to convert a passphrase to a key, use a suitable
+	// package like bcrypt or scrypt.
+	// When decoded the key should be 16 bytes (AES-128) or 32 (AES-256).
 
-	for i := 0; i < runtime.NumCPU(); i++ {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// Never use more than 2^32 random nonces with a given key because of the risk of a repeat.
+	nonce := make([]byte, 12)
+	if _, err := io.ReadFull(rand.Reader, nonce); err != nil {
+		panic(err.Error())
+	}
+
+	aesgcm, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return aesgcm.Seal(nil, nonce, data, nil)
+}
+
+func (client *Client) Start(recording *recording.Recording) {
+	key, _ := hex.DecodeString("6368616e676520746869732070617373776f726420746f206120736563726574")
+	for i := 0; i < 1000; i++ {
 
 		go func(idx int) {
 			addr := net.UDPAddr{Port: 1000 + idx, IP: net.ParseIP("10.11.3.16")}
@@ -47,6 +77,12 @@ func (client *Client) Start(recording *recording.Recording) {
 
 			for {
 				data := recording.GetInMemoryFrame()
+
+				//for y := 0; y < 100; y++ {
+				encryt(key, data)
+				encryt(key, data)
+				//}
+
 				_, err = conn.Write(data)
 
 				if err != nil {
